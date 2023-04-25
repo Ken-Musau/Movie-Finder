08178c4e60020f5411f83d47b99278ce17f83113
Kennedy Musau <kennedy.musauu@gmail.com>
2023-04-25 22:06:53 +0300

    initaialize the dom variables

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 "use strict";
 
-const movieTitle = document.getElementById("movie-title");
-const moviePoster = document.getElementById("movie-poster");
-const searchBtn = document.getElementById("search-btn");
-const movieYear = document.getElementById("year");
 const resultError = document.getElementsByClassName("search-result-container");
-const movieName = "avatar";
+const form = document.querySelector("#search-box");
+const searchBtn = document.getElementById("search-btn");
 
-const url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+const moviePoster = document.getElementById("movie-poster");
+const movieTitle = document.getElementById("movie-title");
+const movieYear = document.getElementById("year");
+const movieRate = document.getElementById("rated");
+const movieReleaseDate = document.getElementById("released");
+const movieGenre = document.getElementById("genre");
+const movieWrite = document.getElementById("writer");
+const movieActors = document.getElementById("actors");
+const moviePlot = document.getElementById('plot');
+// const movieSearchQueryName = "avatar";
 
-const fetchMovie = function () {
+const fetchMovie = function (movieSearchQueryName) {
   if (movieSearchQueryName <= 0) {
     resultError.innerHTML =
       '<h3 class="error-msg">Please enter the correct Movie Name</h3>';
   } else {
-    fetch(url)
+    fetch(`http://www.omdbapi.com/?t=${movieSearchQueryName}&apikey=${key}`)
       .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
+      .then((movie) => {
+        console.log(movie);
+        moviePoster.src = movie.Poster;
+        movieTitle.innerText = movie.Title;
       });
   }
 };
- .
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let movieSearchQueryName = e.target.movie_search_box.value;
+  fetchMovie(movieSearchQueryName);
+  //   form.reset();
+});
