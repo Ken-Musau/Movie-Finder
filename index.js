eaef8edd8eb5363e09fc27f87ac7bf20cb6c8dbc
Kennedy Musau <kennedy.musauu@gmail.com>
2023-04-26 06:12:12 +0300

    worked on hanling errors logic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const resultError = document.getElementsByClassName("search-result-container");
+const resultContainer = document.getElementById("search-result-container");
 const form = document.querySelector("#search-box");
 const searchBtn = document.getElementById("search-btn");
 
@@ -20,31 +20,52 @@ const movieAwards = document.getElementById("awards");
 
 const fetchMovie = function (movieSearchQueryName) {
   if (movieSearchQueryName.length === 0) {
-    resultError.innerHTML =
+    resultContainer.innerHTML =
       '<h3 class="error-msg">Please enter the correct Movie Name</h3>';
   } else {
-    fetch(`http://www.omdbapi.com/?t=${movieSearchQueryName}&apikey=${key}`)
-      .then((response) => response.json())
+    fetch(
+      `http://www.omdbapi.com/?t=${encodeURIComponent(
+        movieSearchQueryName
+      )}&apikey=${key}`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch movie data");
+        }
+        return response.json();
+      })
       .then((movie) => {
-        console.log(movie);
-        moviePoster.src = movie.Poster;
-        movieTitle.innerText = movie.Title;
-        movieYear.innerText = movie.Year;
-        movieRate.innerText = movie.Rated;
-        movieReleaseDate.innerText = movie.Released;
-        movieGenre.innerText = movie.Genre;
-        movieWriter.innerText = movie.Writer;
-        movieActors.innerText = movie.Actors;
-        moviePlot.innerText = movie.Plot;
-        movieLanguage.innerText = movie.Language;
-        movieAwards.innerText = movie.Awards;
+        displayMovie(movie);
+      })
+      .catch((error) => {
+        resultContainer.innerHTML =
+          '<h3 class="error-msg">Failed to fetch movie data. Please try again later.</h3>';
       });
   }
 };
 
+function displayMovie(movie) {
+  moviePoster.src = movie.Poster;
+  movieTitle.innerText = movie.Title;
+  movieYear.innerText = movie.Year;
+  movieRate.innerText = movie.Rated;
+  movieReleaseDate.innerText = movie.Released;
+  movieGenre.innerText = movie.Genre;
+  movieWriter.innerText = movie.Writer;
+  movieActors.innerText = movie.Actors;
+  moviePlot.innerText = movie.Plot;
+  movieLanguage.innerText = movie.Language;
+  movieAwards.innerText = movie.Awards;
+}
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  let movieSearchQueryName = e.target.movie_search_box.value;
-  fetchMovie(movieSearchQueryName);
-  //   form.reset();
+  let movieSearchQueryName = e.target.movie_search_box.value.trim();
+  if (movieSearchQueryName.length === 0) {
+    resultContainer.innerHTML =
+      '<h3 class="error-msg">Please enter a Movie Name</h3>';
+  } else {
+    fetchMovie(movieSearchQueryName);
+    //   form.reset();
+  }
 });
